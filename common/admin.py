5dc056bd829bd87fe265e880174d54d520679b79
Parvej Khan <1999monustp@gmail.com>
2025-08-22 20:43:42 +0530

    registered event model in admin

--- common/admin.py
+++ common/admin.py
@@ -1,16 +1,19 @@
 from django.contrib import admin
-from .models import LatestUpdates, Page
+from .models import LatestUpdates, Page, ConferenceEvent
 
 # Register your models here.
 class LatestUpdateAdmin(admin.ModelAdmin):
-    list_display = ["id", "news", 'live', "order", "priority", "priority_colors"]
+    list_display = ["news", 'live', "order", "priority", "priority_colors"]
     list_editable = ['live', "order", "priority", "priority_colors"]
 
 class PageAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'enabled']
+    list_display = ['name', 'enabled']
     list_editable = ['enabled']
 
-
+class ConfereceEventAdmin(admin.ModelAdmin):
+    list_display = ["name", "prev_date", "updated_date", "is_firm_deadline"]
+    list_editable = ["prev_date", "updated_date", "is_firm_deadline"]
 
 admin.site.register(LatestUpdates, LatestUpdateAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(ConferenceEvent, ConfereceEventAdmin)
